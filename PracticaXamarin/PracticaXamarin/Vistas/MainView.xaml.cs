9286af76228143df79d496094655f826d9183948
Diego Piña Aparicio <Cie_pinha91@hotmail.com>
2017-10-24 10:13:50 +0200

    Agregar archivos de proyecto.

---
+++ PracticaXamarin/PracticaXamarin/Vistas/MainView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PracticaXamarin.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        // Variable contador, se inicializa a 1.
+        int count = 0;
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            MyButton.Clicked += (sender, args) =>
+            {
+               // MyButton.Text = String.Format("{0} clicks!", count++);
+                MessageCount.Text = String.Format("Botón pulsado {0} veces", ++count);
+            };
+        }
+        
+    }
+}
